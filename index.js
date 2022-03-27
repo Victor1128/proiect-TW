c33f2fe399a8f25bd2f77a8359627b8cf47ec38d
Victor Badulescu <victorbadulescu7@gmail.com>
2022-03-27 21:03:41 +0300

    galerie statica, inca nu merge eroarea

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express= require("express");
+const fs=require("fs");
+const { dirname } = require("path");
+const sharp=require("sharp");
 app= express();
 
 app.set("view engine","ejs");
-
 
 app.use("/Resurse", express.static(__dirname+"/Resurse"))
 
@@ -10,19 +12,25 @@ console.log("Director proiect:",__dirname);
 
 app.get(["/", "/index", "/home", "/acasa"], function(req, res){
     //res.sendFile(__dirname+"/index1.html");
-    res.render("pagini/index", {ip:req.ip});
+    res.render("pagini/index", {ip:req.ip, imagini:obImagini.imagini});
 })
+
+app.get("/eroare", function(req, res){
+    randeazaEroare(res,1, "Titlu schimbat");
+});
 
 app.get("/*.ejs", function(req, res){
     //res.sendFile(__dirname+"/index1.html");
-    res.status(403).render("pagini/403");
+    // res.status(403).render("pagini/403");
+    randeazaEroare(res, 403);
 })
 app.get("/*", function(req, res){
     res.render("pagini"+req.url, function(err, rezRender){
         if (err){
             if(err.message.includes("Failed to lookup view")){
                 console.log(err);
-                res.status(404).render("pagini/404");
+                //res.status(404).render("pagini/404");
+                randeazaEroare(res, 404);
             }
             else{
                 
@@ -38,5 +46,54 @@ app.get("/*", function(req, res){
     res.end();
 })
 
-app.listen(3000);
-console.log("A pornit")
+function creeazaImagini(){
+    var buf=fs.readFileSync(__dirname+"/Resurse/json/galerie.json").toString("utf8");
+    obImagini=JSON.parse(buf);
+
+    for(let imag of obImagini.imagini){
+        let nume_imag, extensie;
+        [nume_imag, extensie] = imag.cale_imagine.split(".");
+        let dim_mic=150;
+        let dim_mediu=300;
+
+        imag.mic=`${obImagini.cale_galerie}/mic/${nume_imag}-${dim_mic}.webp`;
+        imag.mediu=`${obImagini.cale_galerie}/mediu/${nume_imag}-${dim_mediu}.png`;
+        imag.mare=`${obImagini.cale_galerie}/${imag.cale_imagine}`;
+
+        if(!fs.existsSync(imag.mic)){
+            sharp(__dirname+"/"+imag.mare).resize(dim_mic).toFile(__dirname+"/"+imag.mic);
+        }
+        if (!fs.existsSync(imag.mediu))
+            sharp(__dirname+"/"+imag.mare).resize(dim_mediu).toFile(__dirname+"/"+imag.mediu);
+        // console.log(imag);
+    }
+}
+
+creeazaImagini();
+
+
+
+
+
+
+function creeazaErori(){
+    let buf=fs.readFileSync(__dirname+"/Resurse/json/erori.json").toString('utf-8');
+    obErori=JSON.parse(buf); //global
+}
+
+creeazaErori();
+
+function randeazaEroare(res, identificator, titlu, text, imagine){
+    var eroare=obErori.erori.find(function(elem){
+        return elem.identificator==identificator;
+    });
+    titlu=titlu || (eroare && eroare.titlu) || "N-avem chiar niciun titlu de eroare";
+    text=text||(eroare&&eroare.text);
+    imagine=imagine||(eroare && (obErori.cale_baza+eroare.imagine));
+    if(eroare && eroare.status)
+        res.status(eroare.identificator).render("pagini/eroare_generala.ejs", {titlu:titlu, text:text, imagine:imagine});
+    else res.render("pagini/eroare_generala.ejs", {titlu:titlu, text:text, imagine:imagine});
+}
+
+app.listen(8080);
+console.log("A pornit");
